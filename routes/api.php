770d2d9c57954567f5c32532444fa98069200b6d
Yara Mohamed <myara1405@gmail.com>
2025-12-03 00:04:00 +0200

    Enhance API: add endpoints for flash-sale flow and hold expiry

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\HoldController;
+use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\PaymentWebhookController;
 use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,3 +13,6 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/products/{id}', [ProductController::class, 'show']);
+Route::post('/holds', [HoldController::class, 'store']);
+Route::post('/orders', [OrderController::class, 'store']);
+Route::post('/payments/webhook', [PaymentWebhookController::class, 'handle']);
